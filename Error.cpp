00ee282f4b19cb44e5af1c0aa65ec8d776f3f638
DirgeWuff <snarkyfluffbutt@gmail.com>
2025-06-05 05:56:34 -0500

    Added basic error and logging handling

---
+++ Error.cpp
@@ -0,0 +1,49 @@
+//
+// Created by DirgeWuff on 6/5/25.
+//
+
+// Some basic error handling/logging utils
+
+#include <fstream>
+#include <chrono>
+#include <sstream>
+#include "raylib.h"
+
+std::string getTime() {
+    const auto currentTime = std::chrono::system_clock::now();
+    const std::time_t currentTimeType = std::chrono::system_clock::to_time_t(currentTime);
+    const std::tm tm = *std::localtime(&currentTimeType);
+
+    std::ostringstream stream;
+    stream << std::put_time(&tm, "%m-%d-%Y %H:%M:%S");
+
+    return stream.str();
+}
+
+void printAndLog(const char* errorMessage) {
+    SetExitKey(KEY_ESCAPE);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText(errorMessage, 5, 5, 15, RED);
+        DrawText("Press Esc to exit.", 5, 20, 15, RED);
+        EndDrawing();
+    }
+
+    if (!std::filesystem::exists("ErrorLog.txt")) {
+        std::ofstream f("ErrorLog.txt");
+
+        if (f.is_open()) {
+            f << getTime() <<  " " << errorMessage << std::endl;
+            f.close();
+        }
+    }
+    else {
+        std::ofstream f;
+
+        f.open("ErrorLog.txt", std::ios::app); // Open in append mode
+        f << getTime() << " " << errorMessage << std::endl;
+        f.close();
+    }
+}
---
+++ Error.h
@@ -0,0 +1,11 @@
+//
+// Created by DirgeWuff on 6/5/25.
+//
+
+#ifndef ERROR_H
+#define ERROR_H
+
+#endif //ERROR_H
+
+void getTime();
+void printAndLog(const char* errorMessage);
